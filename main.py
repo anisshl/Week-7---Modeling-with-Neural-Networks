df09b8b5e3702ba0c9f18e069591adbce85bc60d
anis sahli <anis.sahli@wekey.fr>
2023-04-25 14:04:21 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Chemin vers le fichier des messages
+file_path = 'SMSSpamCollection'
+
+# Lire le fichier et créer un dataframe
+with open(file_path, 'r', encoding='utf-8') as file:
+    # Effectuer le split une fois à la rencontre du premier espace
+    data = [line.strip().split('\t', 1) for line in file]
+
+df = pd.DataFrame(data, columns=['label', 'message'])
+
+print(df.head())
